1d6e2fb308db8f98107c1cfd6c8909b8110a4000
Edward9686 <ink.sans00@mail.ru>
2024-07-26 20:30:10 +0700

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,6 +1,5 @@
 all_done_tasks = '12'
 all_time = '1.5'
 name_course = 'Python'
-time_one_task = '0.125'
-print(float(all_time) / int(all_done_tasks)) # Вычисление времени на одно задание(4)
-print('Курс:', name_course, ', всего задач:', all_done_tasks, ', затрачено часов:', all_time, ', среднее выполнение задания:', time_one_task, 'часа.')
+time_one_tasks = (float(all_time) / int(all_done_tasks))
+print('Курс:', name_course, ', всего задач:', all_done_tasks, ', затрачено часов:', all_time, ', среднее выполнение задания:', time_one_tasks, 'часа.')
